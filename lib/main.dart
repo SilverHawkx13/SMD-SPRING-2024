8e683408a8806d27ce73fa937b65406af3bc7b2a
SilverHawkx13 <smaraza90@gmail.com>
2024-02-05 19:44:39 +0500

    20K-0184 Lab 2

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+ 
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List<String> todo_tasks = [
+    "Task 1",
+    "Task 2",
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Todo List'),
+      ),
+      body: Column(
+        children: [
+          Column(children: [
+            TextField(),
+            ElevatedButton(onPressed: () {}, child: Text('Add'))
+          ]),
+        ],
+      ),
+    );
+  }
+}
